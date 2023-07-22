a7278fc667012808bf3c7dd2238dec7c286667c0
Igor Chaves <igor.cordeiro2016@outlook.com>
2023-07-22 11:16:43 -0300

    feature: lesson-45 accumulative shadows

--- src/components/Lesson45/index.tsx
+++ src/components/Lesson45/index.tsx
@@ -1,6 +1,13 @@
 import React, { useRef } from 'react';
 import * as THREE from 'three';
-import { OrbitControls, useHelper } from '@react-three/drei';
+import {
+  AccumulativeShadows,
+  BakeShadows,
+  OrbitControls,
+  RandomizedLight,
+  useHelper,
+  SoftShadows,
+} from '@react-three/drei';
 import { Perf } from 'r3f-perf';
 import { useFrame } from '@react-three/fiber';
 
@@ -16,35 +23,40 @@ const Lesson45: React.FC = () => {
 
   return (
     <React.Fragment>
+      {/* <SoftShadows focus={3.75} samples={10} size={25} /> */}
+      {/* <BakeShadows />  */}
+      {/* To Stop shadow movement */}
       <color args={['ivory']} attach="background" />
-
       <Perf position="top-left" />
       <OrbitControls makeDefault />
+
+      <AccumulativeShadows position={[0, -0.99, 0]} scale={10}>
+        <RandomizedLight position={[1, 2, 3]} />
+      </AccumulativeShadows>
 
       <directionalLight
         ref={diretionalRef}
         position={[1, 2, 3]}
         intensity={1.5}
         castShadow
+        shadow-mapSize={[1024, 1024]}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-top={5}
+        shadow-camera-right={5}
+        shadow-camera-bottom={-5}
+        shadow-camera-left={-5}
       />
       <ambientLight intensity={0.5} />
-
       <mesh castShadow position-x={-2}>
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
-
       <mesh castShadow position-x={2} scale={1.5} ref={cubeRef}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
-
-      <mesh
-        receiveShadow
-        position-y={-1}
-        rotation-x={-Math.PI * 0.5}
-        scale={10}
-      >
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
